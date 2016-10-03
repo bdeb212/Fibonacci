6cb19d5b6d39bc70c48e2486392b801eb275440d
Jason <sysbender@gmail.com>
2016-10-03 13:18:52 -0400

    fibonacci no recursive

--- AssuranceQualiteFibonacci/Fibonacci.cs
+++ AssuranceQualiteFibonacci/Fibonacci.cs
@@ -9,10 +9,33 @@ namespace AssuranceQualiteFibonacci
     class Fibonacci
     {
         // retourner le Nieme numero de Fibonacci
-        public static Int32 getFn(Int32 n) {
+        public static Int32 getFnR(Int32 n) {
             if (n == 0) return 0;
             if (n == 1) return 1;
             return getFn(n - 1) + getFn(n - 2);
+        }
+
+        // version no recursive
+        public static Int32 getFn(Int32 n)
+        {
+
+            // fn_2 : numero pour n-2
+            // fn_1 : numero pour n-1
+            // fn : numero pour n
+            Int32 fn_2 = 0, fn_1 = 1, fn = 1;
+            for (int i = 2; i <= n; i++)
+            {
+
+                fn = fn_2 + fn_1;
+
+                fn_2 = fn_1;
+                fn_1 = fn;
+
+
+            }
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return fn;
         }
         // retourner le sequence de Fibonacci just'a n
         public static List<Int32> getFlist(Int32 n)
